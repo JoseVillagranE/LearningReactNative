75a7a5badcfcc4639567ea9ce08e406a924b56fd
Jose <josevillagran18@gmail.com>
2022-09-19 02:23:27 -0300

    Upload first solution

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, Linking, Button} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  RefreshControl,
+  FlatList,
+  SectionList,
+  ScrollView,
+} from 'react-native';
 
 const App = () => {
-  const [total, setTotal] = useState(0);
-  const [clickedCounter, setClickedCounter] = useState(0);
-
-  const onclickHandler = () => {
-    setTotal(total + 5);
-    setClickedCounter(clickedCounter + 1);
+  const [Items, setItems] = useState([{title: 'Title 1', data: 'Item 1-1'}]);
+  const [counter, setCounter] = useState(2);
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: 'Item 1-1',
+    },
+  ];
+  const [Refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    setRefreshing(true);
+    setCounter(counter + 1);
+    setItems([
+      ...Items,
+      {
+        title: 'Title ' + counter,
+        data: 'Item ' + counter + '-1',
+      },
+    ]);
+    setRefreshing(false);
   };
 
   return (
-    <View style={styles.body}>
-      <Text style={styles.text}> {total} </Text>
-      <Button title="ADD" onPress={onclickHandler} />
-      <Text style={styles.text}> You clicked {clickedCounter} times </Text>
-    </View>
+    <FlatList
+      keyExtractor={(item, index) => index.toString()}
+      data={Items}
+      renderItem={({item}) => (
+        <View style={styles.body}>
+          <View style={styles.item}>
+            <Text style={styles.text}>{item.title}</Text>
+          </View>
+          <View style={styles.data}>
+            <Text style={styles.text}>{item.data}</Text>
+          </View>
+        </View>
+      )}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+        />
+      }
+    />
+    // <SectionList
+    //   keyExtractor={(item, index) => index.toString()}
+    //   sections={Items}
+    //   renderItem={({item}) => (
+    //     <View style={styles.data}>
+    //       <Text style={styles.text}>{item.data}</Text>
+    //     </View>
+    //   )}
+    //   renderSectionHeader={({section}) => (
+    //     <View style={styles.item}>
+    //       <Text style={styles.text}>{section.title}</Text>
+    //     </View>
+    //   )}
+    //   refreshControl={
+    //     <RefreshControl refreshing={Refreshing} onRefresh={onRefresh} />
+    //   }
+    // />
   );
 };
 
@@ -27,10 +82,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: '#ffffff',
+    color: '#000000',
     fontSize: 20,
     fontStyle: 'italic',
     margin: 10,
+  },
+  item: {
+    backgroundColor: '#4ae1fa',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  data: {
+    backgroundColor: '#ffffff',
+    borderColor: '#000000',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
